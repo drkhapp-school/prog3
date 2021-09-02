26a29adc840f6d683e96239c50ab2c5e856acb6a
darkhappy <darkhappyy@gmail.com>
2021-09-02 00:29:12 -0400

    atelier 1 done

--- atelier1/src/Main.java
+++ atelier1/src/Main.java
@@ -1,4 +1,3 @@
-import java.io.EOFException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.Arrays;
@@ -7,7 +6,7 @@ import java.util.List;
 public class Main {
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         // TODO: Gestion du fichier.
 
         List<String> argList = Arrays.asList(args);
@@ -30,43 +29,54 @@ public class Main {
             }
 
             // length
-            if (Arrays.asList((args)).contains("-l")) {
+            if (argList.contains("-l")) {
                 length = Integer.parseInt(args[argList.indexOf("-l") + 1]);
                 if (length <= 0 || length > file.length() - offset) printUsage();
             } else {
                 length = file.length() - offset;
             }
 
-        } catch (IOException e) {
-            System.out.println(e);
+            printData(file, offset, length);
+        } catch (NumberFormatException | IOException e) {
             printUsage();
-            return;
         }
-        printData(file, offset, length);
     }
 
     public static void printData(RandomAccessFile file, long offset, long length) throws IOException {
         String address;
-        String line = "";
-        String ascii = "";
-        byte newline = 10;
+        int lineCount = (int) (length / 16) + 1;
+        int[] illegal = {10, 13};
+
+        String[] hexArray = new String[lineCount * 16];
+        Arrays.fill(hexArray, "  ");
+
+        char[] ascArray = new char[lineCount * 16];
+        Arrays.fill(ascArray, ' ');
+
+        file.seek(offset);
+        for (int i = (int) offset % 8; i < length; i++) {
+            int value = file.read();
+            hexArray[i] = String.format("%02x", value);
+
+            if (Arrays.stream(illegal).anyMatch(x -> x == value)) ascArray[i] = '.';
+            else ascArray[i] = (char) value;
+        }
 
         System.out.println("\u001B[33m             0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f  ASCII");
-        
-        file.seek(offset);
-        while (file.getFilePointer() < length) {
-            //TODO: make this shit work
+        for (int i = 0; i < lineCount; i++) {
+            StringBuilder hexAddress = new StringBuilder();
+            char[] ascText = new char[16];
 
-            if (file.getFilePointer() % 16 == 0) {
-                address = "0x0000" + (file.getFilePointer() / 16 - 1) + 0;
-
-                if (!line.trim().isEmpty())
-                System.out.printf("\u001B[33m %s \u001B[36m  %s\u001B[37m %s%n", address, line, ascii);
-
-                line = "";
-                ascii = "";
+            for (int j = 0; j < 16; j++) {
+                hexAddress.append(hexArray[i * 16 + j]).append(" ");
             }
+
+            System.arraycopy(ascArray, i * 16, ascText, 0, 16);
+
+            address = "0x0000" + i + "0";
+            System.out.printf("\u001B[33m %s \u001B[36m  %s\u001B[37m %s%n", address, hexAddress, new String(ascText));
         }
+
     }
 
 
