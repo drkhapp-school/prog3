d38da70d6e6502cd2ebcdc941d96eea8fd5e8138
darkhappy <darkhappyy@gmail.com>
2021-09-25 13:37:08 -0400

    formatted

--- pile/ArrayStack.h
+++ pile/ArrayStack.h
@@ -3,33 +3,29 @@
 #define MAX 10
 
 struct ArrayStack {
-    int array[MAX];
-    int topIndex;
+  int array[MAX];
+  int topIndex;
 };
 
 // Initializer la pile
-void init(struct ArrayStack* stack) {
-    stack->topIndex = 0;
-}
+void init(struct ArrayStack *stack) { stack->topIndex = 0; }
 
 // Envoyer un donné dans la pile
-void push(struct ArrayStack* stack, int k) {
-    if (stack->topIndex < MAX)
-        stack->array[stack->topIndex++] = k;
+void push(struct ArrayStack *stack, int k) {
+  if (stack->topIndex < MAX)
+    stack->array[stack->topIndex++] = k;
 }
 
 // Enlever un donné de la pile
-void pop(struct ArrayStack* stack) {
-    if (stack->topIndex)
-        stack->topIndex--;
+void pop(struct ArrayStack *stack) {
+  if (stack->topIndex)
+    stack->topIndex--;
 }
 
 // Donner la valeur du dessus de la pile
-int top(struct ArrayStack* stack) {
-    return stack->topIndex ? stack->array[stack->topIndex - 1] : 0;
+int top(struct ArrayStack *stack) {
+  return stack->topIndex ? stack->array[stack->topIndex - 1] : 0;
 }
 
 // Donner la valeur au dessus de la pile
-int size(struct ArrayStack* stack) {
-    return stack->topIndex;
-}
+int size(struct ArrayStack *stack) { return stack->topIndex; }
--- pile/main.c
+++ pile/main.c
@@ -1,19 +1,20 @@
+#include "ArrayStack.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "ArrayStack.h"
 
 int main() {
-    struct ArrayStack stackA;
+  struct ArrayStack stackA;
 
-    init(&stackA);
-    push(&stackA, 42);
+  init(&stackA);
+  push(&stackA, 42);
+  push(&stackA, 404);
 
-    printf("Pile A -> Dessus: %i, compte: %i\n", top(&stackA), size(&stackA));
-    struct ArrayStack stackB;
+  printf("Pile A -> Dessus: %i, compte: %i\n", top(&stackA), size(&stackA));
+  struct ArrayStack stackB;
 
-    init(&stackB);
-    push(&stackB, 200);
+  init(&stackB);
+  push(&stackB, 200);
 
-    printf("Pile B -> Dessus: %i, compte: %i\n", top(&stackB), size(&stackB));
-    return 0;
+  printf("Pile B -> Dessus: %i, compte: %i\n", top(&stackB), size(&stackB));
+  return 0;
 }
