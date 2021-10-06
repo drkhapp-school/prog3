90f84983576b2fea0e8f3d2f3e9c3761c19fb5a6
darkhappy <darkhappyy@gmail.com>
2021-10-06 13:10:46 -0400

    fuckf orgot comments

--- file/ArrayQueue.hpp
+++ file/ArrayQueue.hpp
@@ -11,10 +11,18 @@ public:
   ArrayQueue() {}
   ~ArrayQueue() {}
 
+  // Ajoute une donnée à la file.
   void push(T data) {};
+
+  // Enleve une donné de la file.
   void pop() {};
   
+  // Retourne la valeur en avant de la file.
   T getFront() {};
+
+  // Retourne la valeur en arrière de la file.
   T getBack() {};
+
+  // Retourne la grosseur de la file.
   size_t getSize() {};
 };
