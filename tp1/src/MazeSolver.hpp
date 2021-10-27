e2d5a862e91c09c93318f58810c50d439366002e
darkhappy <darkhappyy@gmail.com>
2021-10-27 01:17:30 -0400

    tp1: works

--- tp1/src/MazeSolver.hpp
+++ tp1/src/MazeSolver.hpp
@@ -1,26 +1,85 @@
 #include "Maze.hpp"
+#include "Position.hpp"
+#include "Stack.hpp"
 #include "Window.hpp"
-// TODO : Inclusions.
 
 class MazeSolver : public Window {
 private:
   Maze *maze;
-
-  // TODO : Déclarations.
+  Stack<Position *> *stack;
 
 public:
   MazeSolver(Maze *maze) {
     this->maze = maze;
-    // TODO : Instanciations.
+    this->stack = new Stack<Position *>;
+
+    for (int x = 0; x < 53; x++)
+      for (int y = 0; y < 53; y++)
+        if (maze->getSquare(x, y) == Square::ENTRY) {
+          Position *entry = new Position(x, y);
+          setDirections(entry);
+          stack->push(entry);
+        }
   }
 
   ~MazeSolver() {
-    // TODO : Libérations.
     delete maze;
+    delete stack;
+  }
+
+  void setDirections(Position *pos) {
+    pos->dir[0] = maze->getSquare(pos->x, pos->y + 1) != Square::WALL;
+    pos->dir[1] = maze->getSquare(pos->x, pos->y - 1) != Square::WALL;
+    pos->dir[2] = maze->getSquare(pos->x - 1, pos->y) != Square::WALL;
+    pos->dir[3] = maze->getSquare(pos->x + 1, pos->y) != Square::WALL;
   }
 
   void onUpdate() {
-    // TODO : Avancer d'un pas.
+    int direction = 0;
+    int currentX = stack->top()->x;
+    int currentY = stack->top()->y;
+
+    if (maze->getSquare(currentX, currentY) == Square::EXIT)
+      return;
+
+    if (stack->top()->getDirectionCount() == 0)
+      return stack->pop();
+
+    if (stack->top()->getDirectionCount() == 1) {
+      while (!stack->top()->dir[direction])
+        direction++;
+    } else {
+      do {
+        direction = rand() % 4;
+      } while (!stack->top()->dir[direction]);
+    }
+
+    stack->top()->dir[direction] = false;
+    Position *newPosition;
+
+    switch (direction) {
+    case 0:
+      newPosition = new Position(currentX, currentY + 1);
+      setDirections(newPosition);
+      newPosition->dir[1] = false;
+      break;
+    case 1:
+      newPosition = new Position(currentX, currentY - 1);
+      setDirections(newPosition);
+      newPosition->dir[0] = false;
+      break;
+    case 2:
+      newPosition = new Position(currentX - 1, currentY);
+      setDirections(newPosition);
+      newPosition->dir[3] = false;
+      break;
+    case 3:
+      newPosition = new Position(currentX + 1, currentY);
+      setDirections(newPosition);
+      newPosition->dir[2] = false;
+      break;
+    }
+    stack->push(newPosition);
   }
 
   void onRefresh() {
@@ -28,6 +87,6 @@ public:
       for (int y = 0; y < 53; y++)
         drawSquare(maze->getSquare(x, y), x, y);
 
-    // TODO : Afficher le chemin parcouru.
+    drawSquare(Square::PATH, stack->top()->x, stack->top()->y);
   }
 };
--- tp1/src/Position.hpp
+++ tp1/src/Position.hpp
@@ -1,16 +1,24 @@
 class Position {
 public:
-  int x;
-  int y;
-  bool directions[4];
+  unsigned char x;
+  unsigned char y;
+  bool dir[4];
 
-  Position(int x, int y, bool dir[4]) {
+  Position(int x, int y) {
     this->x = x;
     this->y = y;
-    for (int i = 0; i < 4; i++) {
-      directions[i] = dir[i];
-    }
+    for (int i = 0; i < 4; i++)
+      this->dir[i] = false;
   }
 
   ~Position() {}
+
+  int getDirectionCount() {
+    int count = 0;
+    for (int i = 0; i < 4; i++)
+      if (dir[i])
+        count++;
+
+    return count;
+  }
 };
