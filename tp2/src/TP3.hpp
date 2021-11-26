eab91cf2ab29dfe7a96b566a797d162e5aa5e826
darkhappy <darkhappyy@gmail.com>
2021-11-26 17:05:08 -0500

    tp2: getindex

--- tp2/src/TP3.hpp
+++ tp2/src/TP3.hpp
@@ -14,7 +14,6 @@
 
 using namespace std;
 
-inline Folder *root;
 inline Stack<Folder *> *path;
 // AVLTree<int>* selections;
 
@@ -27,20 +26,19 @@ inline Stack<Folder *> *path;
  * @return Position du texte, en pixels, sur l'axe des x
  */
 inline int centerText(int index, string name) {
-  return ((Window::getIconWidth() - Window::getStringWidth(name)) / 2) +
-         (Window::getIconWidth() * (index));
+  return (((Window::getIconWidth() - Window::getStringWidth(name)) / 2) +
+          index);
 }
 
 inline void drawItem(Icon icon, string name, int x, int y,
                      bool selected = false) {
   if (Window::getStringWidth(name) >= Window::getIconWidth()) {
-    name.resize(10);
+    name.resize(14);
     name.replace(name.end(), name.end() - 3, "...");
   }
-  Window::drawIcon(icon, x * Window::getIconWidth(),
-                   y * Window::getIconHeight(), selected);
+  Window::drawIcon(icon, x, y, selected);
   Window::drawString(name, centerText(x, name),
-                     (y + 0.75) * Window::getIconHeight());
+                     y + (Window::getIconHeight() * 0.75));
 }
 
 /**
@@ -52,7 +50,9 @@ inline void drawItem(Icon icon, string name, int x, int y,
  */
 inline int getIndex(const int &x, const int &y) {
   // TODO : Retourner l'indice de l'élément clické
-  return -1;
+  int columns = Window::getWidth() / Window::getIconWidth();
+
+  return (x / Window::getIconWidth() + (y / Window::getIconHeight() * columns));
 }
 
 /**
@@ -60,7 +60,7 @@ inline int getIndex(const int &x, const int &y) {
  */
 inline void onInit() {
   // TODO : Initialisations
-  root = new Folder("/");
+  Folder *root = new Folder("/");
   path = new Stack<Folder *>();
   path->push(root);
   root->createFolder(new Folder("hiii"));
@@ -88,37 +88,30 @@ inline void onRefresh() {
   // Get the current folder's contents
   vector<Folder *> folders = path->top()->getFolders();
   vector<Note *> notes = path->top()->getNotes();
-  int columns = Window::getWidth() / Window::getIconWidth();
-  int index = 0;
-  int widthIndex = 0;
-  int heightIndex = -1;
+  int x = 0;
+  int y = 0;
 
   // Root folder
   folders.insert(folders.begin(), new Folder(".."));
 
   // All folders
   for (Folder *item : folders) {
-    if (!(index % columns)) {
-      heightIndex++;
-      widthIndex = 0;
-    } else {
-      widthIndex++;
+    if (x + Window::getIconWidth() > Window::getWidth()) {
+      y += Window::getIconHeight();
+      x = 0;
     }
-    index++;
-
-    drawItem(Icon::FOLDER, item->getName(), widthIndex, heightIndex);
+    drawItem(Icon::FOLDER, item->getName(), x, y);
+    x += Window::getIconWidth();
   }
 
   // All notes
   for (Note *item : notes) {
-    if (!(index % columns)) {
-      heightIndex++;
-      widthIndex = 0;
-    } else {
-      widthIndex++;
+    if (x + Window::getIconWidth() > Window::getWidth()) {
+      y += Window::getIconHeight();
+      x = 0;
     }
-    index++;
-    drawItem(Icon::NOTE, item->getName(), widthIndex, heightIndex);
+    drawItem(Icon::NOTE, item->getName(), x, y);
+    x += Window::getIconWidth();
   }
 }
 
@@ -134,6 +127,7 @@ inline void onWindowClick(const int &x, const int &y, const bool &button,
                           const bool &ctrl) {
   if (button) {
     // TODO : Click sur un dossier ou une note du dossier actuel
+    int index = getIndex(x, y);
   } else {
     // TODO : Afficher le menu
     Window::showMenu(x, y, Menu::NEW_FOLDER | Menu::NEW_NOTE);
